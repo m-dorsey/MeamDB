f6a4ef7ecb576b219433508b4d4075c109b0774a
m-dorsey <dorseymakenzie@gmail.com>
2022-01-30 18:22:52 -0500

    Initial Commit

---
+++ src/MeamDB/Controller.java
@@ -0,0 +1,4 @@
+package MeamDB;
+
+public class Controller {
+}
---
+++ src/MeamDB/Main.java
@@ -0,0 +1,20 @@
+package MeamDB;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        primaryStage.setTitle("Meamify Music");
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
