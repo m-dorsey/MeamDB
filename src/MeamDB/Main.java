151d87cb70ef8f701adcc7019698ed905e1508c5
Michael Clark <michaelnorrisclark@gmail.com>
2022-03-15 21:39:41 -0400

    Finished the login function aside from actually communicating with the dbms.

--- src/MeamDB/Main.java
+++ src/MeamDB/Main.java
@@ -12,25 +12,44 @@ import java.util.Scanner;
 public class Main {
 
 
-
-    private static void login(){
+    /**
+     *
+     * @return true if valid login, false if the user gives up on logging in.
+     */
+    private static boolean login(){
         //FIXME I feel like having 2 scanners will cause problems.
         // we'll see. Probably want to keep an eye out tho.
         Scanner scan = new Scanner(System.in);
+        String username = "";
+        String password = "";
 
+        boolean validLogin = false;
+        while (!validLogin) {
+            System.out.println("input the username for the account");
+            username = scan.nextLine();
+            System.out.println("input the password for the account.");
+            password = scan.nextLine();
 
+            //now we check to see if this is valid credentials.
+            //if so, we allow them in.
+            //if not, then the following statement executes.
+            System.out.println("invalid login. 'y' to try again. anything else to exit.");
+            String input = scan.nextLine();
+            input = input.toLowerCase();
+            if( input.equals("y")){
+                //we go through the loop again all easy like
+            }else{
+                return false;
+            }
+        }
+
+        //FIXME probably want to actually return a string or string away
+        // with the proper data to do things in the rest of the program.
+        return true;
     }
 
 
     public static void main(String[] args) throws SQLException {
-
-        /*
-
-        gotta somehow figure out how to communicate with the dbms
-
-        but before that, I'll make all the UI shit if
-        I can
-         */
 
         int lport = 5432;
         String rhost = "starbug.cs.rit.edu";
@@ -69,6 +88,88 @@ public class Main {
 
             // Do something with the database....
 
+
+
+
+
+
+            //the user should first be prompted to log in or create
+            // an account
+
+            System.out.println("Input 'login' to log in with your credentials or 'create new account' to create a new account");
+
+
+
+            /*
+            a loop to constantly get input from users
+            while true
+             */
+
+            Scanner scan = new Scanner(System.in);
+            String input = "";
+
+            boolean loggedIn = false;
+
+            //this loop runs forever to get input from the user
+            while( true ) {
+                System.out.println("type 'help' to get a list of commands.");
+
+                input = scan.nextLine();
+                input = input.toLowerCase();
+
+                //TODO we probably want to have a function for each command
+                if (input.equals("login")) {
+                    //in here, we need to handle logging in
+                    boolean successfulLogin = login();
+                    if( successfulLogin ){
+                        loggedIn = true;
+                    }else{
+                        loggedIn = false;
+                    }
+                } else if (input.equals("create new account")) {
+                    //in here, we need to handle creating a new account
+                    //and getting all that data.
+                } else if (input.equals("help")) {
+                    System.out.println("'login' to login to an existing account. You must log in before you execute other commands");
+                    System.out.println("'create new account' to create a new account.");
+                    System.out.println("'create new collection' to create a new collection of songs.");
+                    System.out.println("'view collections' to view all your created collections");
+                    System.out.println("'search song' to search for songs.");
+                    System.out.println("'rename collection' to rename a collection");
+                    System.out.println("'modify collection' to add or remove songs or albums from a collection");
+                    System.out.println("'delete collection' to delete a collection.");
+                    System.out.println("'play song' to play a song.");
+                    System.out.println("'play collection' to play a collection.");
+                    System.out.println("'follow friend' to follow a friend."); //FIXME it isn't specified, but we probably also want to be able to list the follows.
+                    System.out.println("'unfollow friend' to unfollow a friend.");
+                } else if (!loggedIn) {
+                    System.out.println("Error: you must be logged in before executing that command.");
+                } else if (input.equals("create new collection")) {
+
+                } else if (input.equals("view collections")) {
+
+                } else if (input.equals("search song")) {
+
+                } else if (input.equals("rename collection")) {
+
+                } else if (input.equals("modify collection")) {
+
+                } else if (input.equals("delete collection")) {
+
+                } else if (input.equals("play song")) {
+
+                } else if (input.equals("play collection")) {
+
+                } else if (input.equals("follow friend")) {
+
+                } else if (input.equals("unfollow friend")) {
+
+                } else {
+                    System.out.println("That is not a valid command.");
+                }
+
+
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -81,86 +182,6 @@ public class Main {
                 session.disconnect();
             }
         }
-
-
-
-
-        //the user should first be prompted to log in or create
-        // an account
-
-        System.out.println("Input 'login' to log in with your credentials or 'create new account' to create a new account");
-
-
-
-        /*
-        a loop to constantly get input from users
-        while true
-         */
-
-        Scanner scan = new Scanner(System.in);
-        String input = "";
-
-        boolean loggedIn = false;
-
-        //this loop runs forever to get input from the user
-        while( true ){
-            System.out.println("type 'help' to get a list of commands.");
-
-            input = scan.nextLine();
-            input = input.toLowerCase();
-
-            //TODO we probably want to have a function for each command
-            if( input.equals("login") ){
-                //in here, we need to handle logging in
-            }else if( input.equals("create new account")){
-                //in here, we need to handle creating a new account
-                //and getting all that data.
-            }else if ( input.equals("help") ){
-                System.out.println("'login' to login to an existing account. You must log in before you execute other commands");
-                System.out.println("'create new account' to create a new account.");
-                System.out.println("'create new collection' to create a new collection of songs.");
-                System.out.println("'view collections' to view all your created collections");
-                System.out.println("'search song' to search for songs.");
-                System.out.println("'rename collection' to rename a collection");
-                System.out.println("'modify collection' to add or remove songs or albums from a collection");
-                System.out.println("'delete collection' to delete a collection.");
-                System.out.println("'play song' to play a song.");
-                System.out.println("'play collection' to play a collection.");
-                System.out.println("'follow friend' to follow a friend."); //FIXME it isn't specified, but we probably also want to be able to list the follows.
-                System.out.println("'unfollow friend' to unfollow a friend.");
-            }else if( !loggedIn ){
-                System.out.println("Error: you must be logged in before executing that command.");
-            }else if( input.equals("create new collection")){
-
-            }else if( input.equals("view collections")){
-
-            }else if( input.equals("search song")){
-
-            }else if( input.equals("rename collection")){
-
-            }else if( input.equals("modify collection")){
-
-            }else if( input.equals("delete collection")){
-
-            }else if( input.equals("play song")){
-
-            }else if( input.equals("play collection")){
-
-            }else if( input.equals("follow friend")){
-
-            }else if( input.equals("unfollow friend")){
-
-            }else{
-                System.out.println("That is not a valid command.");
-            }
-
-
-
-
-
-
-        }
-
-
     }
 }
+
