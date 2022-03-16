5359db745b18cb6855d389bcced657b1b158c8e6
Michael Clark <michaelnorrisclark@gmail.com>
2022-03-15 21:21:51 -0400

    Finished the help and most of the generic input getting.

--- src/MeamDB/Main.java
+++ src/MeamDB/Main.java
@@ -5,13 +5,24 @@ import java.util.Scanner;
 
 public class Main {
 
+
+
+    private static void login(){
+        //FIXME I feel like having 2 scanners will cause problems.
+        // we'll see. Probably want to keep an eye out tho.
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+
+
     public static void main(String[] args) {
 
         /*
 
         gotta somehow figure out how to communicate with the dbms
 
-        but before that, I, Michael, will make all the UI shit if
+        but before that, I'll make all the UI shit if
         I can
          */
 
@@ -32,11 +43,15 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
         String input = "";
+
+        boolean loggedIn = false;
+
         //this loop runs forever to get input from the user
         while( true ){
             System.out.println("type 'help' to get a list of commands.");
 
             input = scan.nextLine();
+            input = input.toLowerCase();
 
             //TODO we probably want to have a function for each command
             if( input.equals("login") ){
@@ -57,7 +72,30 @@ public class Main {
                 System.out.println("'play collection' to play a collection.");
                 System.out.println("'follow friend' to follow a friend."); //FIXME it isn't specified, but we probably also want to be able to list the follows.
                 System.out.println("'unfollow friend' to unfollow a friend.");
+            }else if( !loggedIn ){
+                System.out.println("Error: you must be logged in before executing that command.");
+            }else if( input.equals("create new collection")){
 
+            }else if( input.equals("view collections")){
+
+            }else if( input.equals("search song")){
+
+            }else if( input.equals("rename collection")){
+
+            }else if( input.equals("modify collection")){
+
+            }else if( input.equals("delete collection")){
+
+            }else if( input.equals("play song")){
+
+            }else if( input.equals("play collection")){
+
+            }else if( input.equals("follow friend")){
+
+            }else if( input.equals("unfollow friend")){
+
+            }else{
+                System.out.println("That is not a valid command.");
             }
 
 
